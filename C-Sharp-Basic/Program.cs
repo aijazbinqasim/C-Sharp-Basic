54ef589a5a7b6e9bbfc6ec5e2f2dc6d5aeb780bd
aijazbinqasim <aijazbinqasim@hotmail.com>
2021-10-15 19:13:40 +0500

    Add project files.

---
+++ C-Sharp-Basic/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using C_Sharp_Basic.classes;
+using System.Collections.Generic;
+
+namespace C_Sharp_Basic
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // following is called object initialer syntax
+            var php = new Book()
+            {
+                Id = 1,
+                Title = "Php",
+                Price = 3000,
+                Languages = new List<string>()
+                {
+                    "English",
+                    "Urdu"
+                }
+            };
+
+            var js = new Book()
+            {
+                Id = 2,
+                Title = "JavaScript",
+                Languages = new List<string>()
+                {
+                    "English",
+                    "Urdu",
+                    "Sindhi"
+                }
+            };
+
+            Console.WriteLine("Book1: " + php.GetBookInfo());
+
+            Console.WriteLine("Book2: " + js.GetBookInfo());
+        }
+    }
+}
---
+++ C-Sharp-Basic/classes/Book.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C_Sharp_Basic.classes
+{
+    class Book
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public IList<string> Languages { get; set; }
+        public decimal? Price { get; set; }
+
+        //public Book(int id, string title, IList<string> languages, decimal? price)
+        //{
+        //    Id = id;
+        //    Title = title;
+        //    Languages = languages;
+        //    Price = price;
+        //}
+
+        public string GetBookInfo()
+        {
+            var lang = string.Empty;
+
+            foreach (var language in Languages)
+            {
+                lang += $"{language}";
+                lang += ",";
+            }
+
+            return $"Id: {Id} Title: {Title} Languages: {lang} Price: {Price ?? 0}";
+        }
+    }
+}
+
+// class contains data (properties & fields) and behavior (functions/methods) to operate on that data
